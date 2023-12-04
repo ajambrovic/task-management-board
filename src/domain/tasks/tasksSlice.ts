c5cbdb2b6a788635b63e460098c826523343b5d6
Andrija Jambrovic <andrija.jambrovic@gmail.com>
2023-12-04 01:37:29 +0100

    Added task deletion to redux

--- src/domain/tasks/tasksSlice.ts
+++ src/domain/tasks/tasksSlice.ts
@@ -2,39 +2,41 @@ import {NetworkRequestStatus} from 'domain/networkRequest/networkRequestModel';
 import {type TaskModel, TaskPriority, TaskStatus, type TasksReduxModel} from './tasksModel';
 import {type PayloadAction, createSlice} from '@reduxjs/toolkit';
 
+const byIdDummyData = {
+  '1': {
+    id: '1',
+    name: 'First task',
+    status: TaskStatus.ToDo,
+    description: 'ToDo',
+    dueByTimestamp: 1702011443720,
+    priority: TaskPriority.High,
+    assignedTeamMember: 'Pero Perić',
+  },
+  '2': {
+    id: '2',
+    name: 'Second task',
+    status: TaskStatus.InProgress,
+    description: 'InProgress',
+    dueByTimestamp: 1702011443720,
+    priority: TaskPriority.Medium,
+    assignedTeamMember: 'Ivan Horvart',
+  },
+  '3': {
+    id: '3',
+    name: 'Third task',
+    status: TaskStatus.Completed,
+    description: 'Completed',
+    dueByTimestamp: 1701511443720,
+    priority: TaskPriority.Low,
+    assignedTeamMember: 'Pero Perić',
+  },
+};
+
 const initialState: TasksReduxModel = {
   networkRequestStatus: NetworkRequestStatus.Success,
   initialLoad: true,
   ids: ['1', '2', '3'],
-  byId: {
-    '1': {
-      id: '1',
-      name: 'First task',
-      status: TaskStatus.ToDo,
-      description: 'ToDo',
-      dueByTimestamp: 1702011443720,
-      priority: TaskPriority.High,
-      assignedTeamMember: 'Pero Perić',
-    },
-    '2': {
-      id: '2',
-      name: 'Second task',
-      status: TaskStatus.InProgress,
-      description: 'InProgress',
-      dueByTimestamp: 1702011443720,
-      priority: TaskPriority.Medium,
-      assignedTeamMember: 'Ivan Horvart',
-    },
-    '3': {
-      id: '3',
-      name: 'Third task',
-      status: TaskStatus.Completed,
-      description: 'Completed',
-      dueByTimestamp: 1701511443720,
-      priority: TaskPriority.Low,
-      assignedTeamMember: 'Pero Perić',
-    },
-  },
+  byId: byIdDummyData,
 };
 
 export const tasksSlice = createSlice({
@@ -47,6 +49,14 @@ export const tasksSlice = createSlice({
     createTask: (state, action: PayloadAction<TaskModel>) => {
       state.byId[action.payload.id] = action.payload;
       state.ids.push(action.payload.id);
+    },
+    deleteTask: (state, action: PayloadAction<TaskModel['id']>) => {
+      const index = state.ids.findIndex(id => id === action.payload);
+      const elementFound = index !== -1;
+      if (elementFound) {
+        state.ids.splice(index, 1);
+        delete state.byId[action.payload];
+      }
     },
   },
 });
