2cf3c84652b90e66572c0d29589084f6e95835af
Andrija Jambrovic <andrija.jambrovic@gmail.com>
2023-12-04 15:36:42 +0100

    Implemented persisting to local storage

--- src/domain/tasks/tasksSlice.ts
+++ src/domain/tasks/tasksSlice.ts
@@ -1,42 +1,12 @@
 import {createSlice, type PayloadAction} from '@reduxjs/toolkit';
 import {NetworkRequestStatus} from 'domain/networkRequest/networkRequestModel';
-import {TaskPriority, TaskStatus, type TaskModel, type TasksReduxModel} from './tasksModel';
-
-const byIdDummyData = {
-  '1': {
-    id: '1',
-    name: 'First task',
-    status: TaskStatus.ToDo,
-    description: 'ToDo',
-    dueByTimestamp: 1702011443720,
-    priority: TaskPriority.High,
-    assignedTeamMember: 'Pero Perić',
-  },
-  '2': {
-    id: '2',
-    name: 'Second task',
-    status: TaskStatus.InProgress,
-    description: 'InProgress',
-    dueByTimestamp: 1702011443720,
-    priority: TaskPriority.Medium,
-    assignedTeamMember: 'Ivan Horvart',
-  },
-  '3': {
-    id: '3',
-    name: 'Third task',
-    status: TaskStatus.Completed,
-    description: 'Completed',
-    dueByTimestamp: 1701511443720,
-    priority: TaskPriority.Low,
-    assignedTeamMember: 'Pero Perić',
-  },
-};
+import {type TaskModel, type TasksReduxModel, type TaskStatus} from './tasksModel';
 
 const initialState: TasksReduxModel = {
   networkRequestStatus: NetworkRequestStatus.Success,
   initialLoad: true,
-  ids: ['1', '2', '3'],
-  byId: byIdDummyData,
+  ids: [],
+  byId: {},
 };
 
 export const tasksSlice = createSlice({
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,9 +1,10 @@
+import {combineReducers} from '@reduxjs/toolkit';
 import {tasksReducer} from 'domain/tasks/tasksSlice';
 import {counterReducer} from './counter/slice';
 
-const rootReducer = {
+const rootReducer = combineReducers({
   counter: counterReducer,
   tasks: tasksReducer,
-};
+});
 
 export default rootReducer;
