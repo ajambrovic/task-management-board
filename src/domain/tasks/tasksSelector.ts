7a4fa4fb4a257056302f804841e6834f020df1ca
Andrija Jambrovic <andrija.jambrovic@gmail.com>
2023-12-13 19:37:53 +0100

    Cache is good enough

--- src/domain/tasks/tasksSelector.ts
+++ src/domain/tasks/tasksSelector.ts
@@ -1,24 +1,27 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { type RootState } from 'redux/store';
-import { type TaskModel, type TaskStatus } from './tasksModel';
+import { type TaskModel } from './tasksModel';
 
 export const selectTaskIndex = (state: RootState, taskId: TaskModel['id']) => {
   return selectTasksIds(state).findIndex((id) => id === taskId);
 };
 
-export const selectTaskIdsByTaskStatus = (state: RootState, taskStatus: TaskStatus) => {
-  const tasks = selectTasksData(state);
-  const taskIds = selectTasksIds(state);
-  const filteredTasks = Object.values(tasks).filter((task) => task.status === taskStatus);
-  // Sort tasks based on the order of taskIds
-  const sortedTaskIds: TaskModel['id'][] = taskIds
-    .map((taskId) => {
-      const task = filteredTasks.find((t) => t.id === taskId);
-      return task != null ? task.id : undefined;
-    })
-    .filter((taskId): taskId is TaskModel['id'] => taskId !== undefined);
+export const selectTaskIdsByTaskStatus = createSelector(
+  [selectTasksData, (_, taskStatus) => taskStatus, selectTasksIds, (_, taskIds) => taskIds],
+  (tasks, taskStatus, taskIds) => {
+    // Filter tasks based on taskStatus
+    const filteredTasks = Object.values(tasks).filter((task) => task.status === taskStatus);
 
-  return sortedTaskIds;
-};
+    const sortedTaskIds: TaskModel['id'][] = taskIds
+      .map((taskId) => {
+        const task = filteredTasks.find((t) => t.id === taskId);
+        return task != null ? task.id : undefined;
+      })
+      .filter((taskId): taskId is TaskModel['id'] => taskId !== undefined);
+
+    return sortedTaskIds;
+  },
+);
 
 export const selectCurrentTaskSearchQuery = (state: RootState) => {
   return selectTasks(state).searchQuery;
